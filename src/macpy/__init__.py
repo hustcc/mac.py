fc8792759b2fec7b2d76958e3c0cef9f9e899696
hustcc <i@atool.org>
2016-04-28 13:29:56 +0800

    update __init__

--- src/macpy/__init__.py
+++ src/macpy/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-# from __future__ import absolute_import
+from __future__ import absolute_import
 
-from Mac import *
+from .Mac import *
 
 __version__ = '1.0.0'
 __license__ = 'MIT'
