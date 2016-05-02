f940de0c1b35a254766a5035b1dde0988566016f
aTool <i@atool.org>
2016-05-02 17:15:16 +0800

    fix #1

--- test/test_unitest.py
+++ test/test_unitest.py
@@ -1,8 +1,10 @@
 #-*-coding: utf-8 -*-
 
-import unittest, time
-import sys, random
-from macpy import Mac
+import unittest
+try:
+    from src.macpy import Mac
+except:
+    from macpy import Mac
 
 
 class TestCase(unittest.TestCase):
@@ -20,15 +22,17 @@ class TestCase(unittest.TestCase):
         r_1 = self.mac.search('00016C')
         r_2 = self.mac.search('00:01:6C:06:A6:29')
         r_3 = self.mac.search('00-01-6C-06-A6-29')
-        r_4 = self.mac.search('00')
-        r_5 = self.mac.search('')
+        r_4 = self.mac.search('00016c')
+        r_5 = self.mac.search('00')
+        r_6 = self.mac.search('')
         
         self.assertEqual(r_1, r_2)
         self.assertEqual(r_2, r_3)
-        self.assertEqual(r_3, {'re': 'Brea  CA  92821', 'com': 'FOXCONN', 'addr': '105 S Puente St.', 'co': 'US'})
+        self.assertEqual(r_3, r_4)
+        self.assertEqual(r_4, {'re': 'Brea  CA  92821', 'com': 'FOXCONN', 'addr': '105 S Puente St.', 'co': 'US'})
         
-        self.assertEqual(r_4, None)
         self.assertEqual(r_5, None)
+        self.assertEqual(r_6, None)
 
 if __name__ =='__main__':
     unittest.main()
